269f9a801f104736a56379c67185b09445cd7199
rmajuri <rmajuri@govexec.com>
2022-03-22 16:42:25 -0400

    Set up log in

--- components/Nav.js
+++ components/Nav.js
@@ -1,13 +1,17 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 import { AppBar, IconButton, Toolbar, Typography, Drawer, List, ListItem, ListItemIcon, ListItemText } from "@mui/material"
 import { styled } from '@mui/material/styles';
 import MenuIcon from '@mui/icons-material/Menu';
 import HomeIcon from '@mui/icons-material/Home';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { useRouter } from 'next/router';
+import AuthenticationContext from '../context/AuthenticationContext'
 
 const Nav = () => {
     const [toggle, setToggle] = useState(false)
     const router = useRouter();
+
+    const {user} = useContext(AuthenticationContext)
 
     const toggleDrawer = value => event => {
         if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
@@ -34,6 +38,18 @@ const Nav = () => {
                                     <ListItemIcon><HomeIcon /></ListItemIcon>
                                     <ListItemText primary="Home" />
                                 </ListItem>
+
+                                {user ? (
+                                    <ListItem button onClick={() => router.push('/logout')}>
+                                        <ListItemIcon><AccountCircleIcon /></ListItemIcon>
+                                        <ListItemText primary='Sign Out' />
+                                    </ListItem>
+                                    ) : (
+                                        <ListItem button onClick={() => router.push('/login')}>
+                                            <ListItemIcon><AccountCircleIcon /></ListItemIcon>
+                                            <ListItemText primary='Sign In' />
+                                        </ListItem>
+                                    )}
                             </List>
                         </div>
                     </Drawer>
--- pages/account/login.js
+++ pages/account/login.js
@@ -1,21 +1,53 @@
 import Layout from '../components/Layout';
-// import { Grid, Avatar, Card, CardHeader } from '@mui/material';
+import { Button, Card, CardContent, TextField, Typography } from '@mui/material';
+import { useState, useContext } from 'react'
+import Layout from '../../components/Layout'
+import AuthenticationContext from '../../context/AuthenticationContext'
+import Link from 'next/link'
 
-export default function Login({  }) {
+const Login = ({  }) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const {login} = useContext(AuthenticationContext)
+
+  const submitHandler = e => {
+  	e.preventDefault();
+  	login({username, password})
+  }
 
   return (
     <Layout>
+    <div>
+      <div>
+        <Typography variant='h3'>Login</Typography>
+        <Card>
+          <CardContent>
+            <form onSubmit={submitHandler}>
+              <div>
+                <TextField label='Username' fullWidth onChange={e => setUsername(e.target.value)} value={username} />
+              </div>
 
+              <div>
+                <TextField label='Password' inputProps={{ 'type': 'password' }} fullWidth onChange={e => setPassword(e.target.value)} value={password} />
+              </div>
+
+              <div>
+                <Button variant='contained' color='primary' type='submit'>Login</Button>
+              </div>
+
+              <div>
+                <Link href='account/register'>
+                  <a>Don't have an account? Sign Up</a>
+                </Link>
+              </div>
+            </form>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
     </Layout>
   )
 }
 
-// export async function getServerSideProps() {
-//   const { data } = await axios.get('http://localhost:8000/categories')
-
-//   return {
-//     props: {
-//       categories: data.results
-//     }
-//   }
-// }
+export default Login
